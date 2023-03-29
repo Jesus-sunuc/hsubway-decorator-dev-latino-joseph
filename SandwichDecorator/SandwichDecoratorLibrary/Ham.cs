dac89cba052c29af565ccd088a5d3e567b9af244
Nathan D Cabrera <nathandaniel93@hotmail.com>
2023-03-28 21:09:44 -0600

    Lettuce, Bacon and Ham

--- SandwichDecorator/SandwichDecoratorLibrary/Ham.cs
+++ SandwichDecorator/SandwichDecoratorLibrary/Ham.cs
@@ -8,21 +8,28 @@ namespace SandwichDecoratorLibrary
 {
     public class Ham : ITopping
     {
+        ISandwich Sandwich;
+        ITopping Topping;
+        decimal price = .60m;
         public Ham(ITopping topping)
         {
+            Topping = topping;
 
+            price += topping.GetPrice();
         }
         public Ham(ISandwich sandwich)
         {
+            Sandwich = sandwich;
 
+            price += sandwich.GetPrice();
         }
         public decimal GetPrice()
         {
-            return 0;
+            return price;
         }
         public string GetDescription()
         {
-            return "";
+            return "+ ham";
         }
     }
 }
