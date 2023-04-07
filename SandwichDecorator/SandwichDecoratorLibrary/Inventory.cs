3ca6aac91b2a40f026c0c0e9cd8b7d8d7dc56f74
jesus sunuc <mariano.code4guate@gmail.com>
2023-04-07 16:33:16 -0600

    exceptions fixed

--- SandwichDecorator/SandwichDecoratorLibrary/Inventory.cs
+++ SandwichDecorator/SandwichDecoratorLibrary/Inventory.cs
@@ -10,8 +10,8 @@ namespace SandwichDecoratorLibrary
     public class Inventory
     {
         int sandwichStock = 10;
-        int breadStock = 50;
-        int toppingStock = 2;
+        int breadStock = 3;
+        int toppingStock = 4000;
 
         //Keeps track of how much money was made in the day
         decimal dailyRevenue = 0;
@@ -48,150 +48,169 @@ namespace SandwichDecoratorLibrary
 
         public void SellBLT(Bread bread)
         {
-            if (BLT <= 0 || (bread == Bread.rye && rye < 2) || (bread == Bread.wheat && wheat < 2) || (bread == Bread.white && white < 2) || breadStock <= 0 || sandwichStock <= 0)
+            if (breadStock <= 0)
             {
-                throw new MissingIngredientException("Cannot sell BLT sandwich due to missing of bread");
+                throw new MissingIngredientException("Cannot sell BLT sandwich or bread due to missing of bread");
             }
 
-            BLT--;
-            if (bread == Bread.rye) { rye = rye - 2; }
-            else if (bread == Bread.wheat) { wheat = wheat - 2; }
-            else { white = white - 2; }
+            breadStock -= 2;
+            //sandwichStock--;
+            BLT++;
+            if (bread == Bread.rye) { rye += 2; }
+            else if (bread == Bread.wheat) { wheat += 2; }
+            else { white += 2; }
         }
 
         public void SellPBJ(Bread bread)
         {
-            if (PBJ <= 0 || (bread == Bread.rye && rye < 2) || (bread == Bread.wheat && wheat < 2) || (bread == Bread.white && white < 2) || breadStock <= 0 || sandwichStock <= 0)
+            if (breadStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell BLT sandwich due to missing bread.");
             }
-
-            PBJ--;
-            if (bread == Bread.rye) { rye = rye - 2; }
-            else if (bread == Bread.wheat) { wheat = wheat - 2; }
-            else { white = white - 2; }
+            breadStock -= 2;
+            //sandwichStock--;
+            PBJ++;
+            if (bread == Bread.rye) { rye += 2; }
+            else if (bread == Bread.wheat) { wheat += 2; }
+            else { white += 2; }
         }
         public void SellChicken(Bread bread)
         {
-            if (Chicken <= 0 || (bread == Bread.rye && rye < 2) || (bread == Bread.wheat && wheat < 2) || (bread == Bread.white && white < 2) || breadStock <= 0 || sandwichStock <= 0)
-            {
+            if (breadStock <= 0)
+            {;
                 throw new MissingIngredientException("Cannot sell BLT sandwich due to missing bread.");
             }
 
-            Chicken--;
-            if (bread == Bread.rye) { rye = rye - 2; }
-            else if (bread == Bread.wheat) { wheat = wheat - 2; }
-            else { white = white - 2; }
+            breadStock -= 2;
+            //sandwichStock--;
+            Chicken++;
+            if (bread == Bread.rye) { rye += 2; }
+            else if (bread == Bread.wheat) { wheat += 2; }
+            else { white += 2; }
         }
         public void SellBacon()
         {
-            if (bacon <= 0)
+            if (toppingStock <= 0)
             {
-                throw new MissingIngredientException("Cannot sell bacon due to missing ingredient.");
+                throw new MissingIngredientException("Cannot sell bacon due to missing bacon.");
             }
+            toppingStock--;
 
-            bacon--;
-
+            bacon++;
         }
 
         public void SellHam()
         {
-            if (ham <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing ham.");
             }
-            ham--;
 
+            toppingStock--;
+            ham++;
         }
 
         public void SellMustard()
         {
-            if (mustard <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing mustard.");
             }
-            mustard--;
+
+            toppingStock--;
+            mustard++;
         }
 
         public void SellBBQ()
         {
-            if (BBQ <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing BBQ topping.");
             }
-            BBQ--;
+
+            toppingStock--;
+            BBQ++;
         }
 
         public void SellCheese()
         {
-            if (cheese <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing cheese topping.");
             }
-            cheese--;
+
+            toppingStock--;
+            cheese++;
         }
 
         public void SellLettuce()
         {
-            if (lettuce <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing lettuce topping.");
             }
-            lettuce--;
+
+            toppingStock--;
+            lettuce++;
         }
 
         public void SellMayo()
         {
-            if (mayo <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing mayo topping.");
             }
-            mayo--;
+
+            toppingStock--;
+            mayo++;
         }
 
         public void SellTomato()
         {
-            if (tomato <= 0)
+            if (toppingStock <= 0)
             {
                 throw new MissingIngredientException("Cannot sell the sandwich due to missing tomato topping.");
             }
-            tomato--;
+
+            toppingStock--;
+            tomato++;
         }
 
         public string Report()
         {
             return $"Sales made \n" +
-                $"BLT sandwiches: {sandwichStock - BLT}\n" +
-                $"PBJ sandwiches: {sandwichStock - PBJ}\n" +
-                $"Chicken sandwiches: {sandwichStock - Chicken}\n" +
-                $"White bread: {breadStock - white}\n" +
-                $"Wheat bread: {breadStock - wheat}\n" +
-                $"Rye bread: {breadStock - rye} \n" +
-                $"Bacon topping: {toppingStock - bacon}\n" +
-                $"Ham topping: {toppingStock - ham}\n" +
-                $"Mustard topping: {toppingStock - mustard}\n" +
-                $"BBQ topping: {toppingStock - BBQ}\n" +
-                $"Cheese topping: {toppingStock - cheese}\n" +
-                $"Lettuce topping: {toppingStock - lettuce}\n" +
-                $"Mayo topping: {toppingStock - mayo}\n" +
-                $"Tomato topping: {toppingStock- tomato}";
+                $"BLT sandwiches: {BLT}\n" +
+                $"PBJ sandwiches: {PBJ}\n" +
+                $"Chicken sandwiches: {Chicken}\n" +
+                $"White bread: {white}\n" +
+                $"Wheat bread: {wheat}\n" +
+                $"Rye bread: {rye} \n" +
+                $"Bacon topping: {bacon}\n" +
+                $"Ham topping: {ham}\n" +
+                $"Mustard topping: {mustard}\n" +
+                $"BBQ topping: {BBQ}\n" +
+                $"Cheese topping: {cheese}\n" +
+                $"Lettuce topping: {lettuce}\n" +
+                $"Mayo topping: {mayo}\n" +
+                $"Tomato topping: {tomato}" +
+                $"{breadStock}";
         }
         public void ResetStock()
         {
-            BLT = sandwichStock;
-            PBJ = sandwichStock;
-            Chicken = sandwichStock;
-            white = breadStock;
-            wheat = breadStock;
-            rye = breadStock;
-            bacon = toppingStock;
-            ham = toppingStock;
-            mustard = toppingStock;
-            BBQ = toppingStock;
-            cheese = toppingStock;
-            lettuce = toppingStock;
-            mayo = toppingStock;
-            tomato = toppingStock;
+            BLT = 0;
+            PBJ = 0;
+            Chicken = 0;
+            white = 0;
+            wheat = 0;
+            rye = 0;
+            bacon = 0;
+            ham = 0;
+            mustard = 0;
+            BBQ = 0;
+            cheese = 0;
+            lettuce = 0;
+            mayo = 0;
+            tomato = 0;
         }
 
         public void AddRevenue(decimal money)
